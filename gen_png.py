58151e6a710d9192ad9f4b12c7ae5e06b1d06733
Riesi <riesi@opentrash.com>
2019-12-05 18:07:57 +0100

    [util] the python learned a new trick!
    We can now generate dedicated frogs only by passing their name as arguments,

--- gen_png.py
+++ gen_png.py
@@ -2,8 +2,17 @@
 import glob
 import ntpath
 import subprocess
+import sys
+import os
 
-files = [f for f in glob.glob("./**/*.svg", recursive=True)]
+files = list()
+if len(sys.argv) == 1:
+    files = [f for f in glob.glob("./**/*.svg", recursive=True)]
+else:
+    for f in sys.argv:
+        if os.path.exists("./svg/"+f+".svg"):
+            files.append("./svg/"+f+".svg")
+
 sizes = [16, 32, 64, 128, 256, 512, 1024, 4096]
 
 for f in files:
